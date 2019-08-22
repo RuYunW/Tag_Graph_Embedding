17ad0bdaa83d761baf17fcd5fd57ebadcd86109b
Ash <ruyunw@163.com>
2019-08-22 20:21:09 +0800

    Initial commit

---
+++ model.py
@@ -0,0 +1,19 @@
+'''
+Tag-graph embedding generation algorithm
+'''
+
+from utils import load_data,LSTM
+
+x = load_data()
+order = set()
+
+
+for k in range(5):  # hop数
+    for m in range(3):  # 节点个数
+        h[][] =
+        h[][] =
+        order = h[][] + h[][]
+        for u in order:
+             h[][] = LSTM(h[][])
+        h[][] = Sigma()
+z[] = h[][]
---
+++ utils.py
@@ -0,0 +1,12 @@
+
+def load_data(cites_path = './data/cora.cites',content_path = './data/cora.content'):
+    with open(cites_path,'r') as f1:
+        lines = f1.readlines()
+
+        '''
+        etc……
+        '''
+    x = []
+
+
+def LSTM():
