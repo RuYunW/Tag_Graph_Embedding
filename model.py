2ab87e32f765765af2fed06a128e732a73af8222
Ash <ruyunw@163.com>
2019-08-22 23:28:17 +0800

    made a framework, specific details need to be done

--- model.py
+++ model.py
@@ -4,15 +4,18 @@ Tag-graph embedding generation algorithm
 
 from utils import load_data,LSTM
 
+# Initialize
 x = load_data()
-order = set()
+order = []  # 创建一个空集合
+K = 5
 
 
-for k in range(5):  # hop数
-    for m in range(3):  # 节点个数
-        h[][] =
-        h[][] =
-        order = h[][] + h[][]
+h[m][0] = x[m]
+for k in range(K):  # hop数  K
+    for m in range(len(x)):  # 节点个数  M
+        h[][k] = Virtualized()
+        h[][k] = Virtualized()
+        order = h[][k] + h[][k]
         for u in order:
              h[][] = LSTM(h[][])
         h[][] = Sigma()
