7786c7bc5c6a2c92dca55b2cf201f33e82284aa9
汝芸 <ruyunw@163.com>
2019-08-24 23:39:50 +0800

    h

--- model.py
+++ model.py
@@ -2,21 +2,27 @@
 Tag-graph embedding generation algorithm
 '''
 
-from utils import load_data,LSTM
+from utils import load_data
 
 # Initialize
 x = load_data()
 order = []  # 创建一个空集合
 K = 5
-h = []
+h = [[] for i in range(K+1)]
+# print(h)
+# print(x[0][0])  # [[0,...,0],[...],...,[...]]
+for i in x:
+    h[0].append(i)
+# print(h[0])
 
-h[:][0] = x[:]
-for k in range(K):  # hop数  K
-    for m in range(len(x)):  # 节点个数  M
-        h[][k] = Virtualized()
-        h[][k] = Virtualized()
-        order = h[][k] + h[][k]
-        for u in order:
-             h[][] = LSTM(h[][])
-        h[][] = Sigma()
-z[] = h[][]
+print(len(h[0]))  # len(h[0]=2708)
+
+# for k in range(K):  # hop数  K
+#     for m in range(len(x)):  # 节点个数  M
+#         h[][k] = Virtualized()
+#         h[][k] = Virtualized()
+#         order = h[][k] + h[][k]
+#         for u in order:
+#              h[][] = LSTM(h[][])
+#         h[][] = Sigma()
+# z[] = h[][]
--- utils.py
+++ utils.py
@@ -1,13 +1,15 @@
 
 def load_data(cites_path = './data/cora.cites',content_path = './data/cora.content'):
-    with open(cites_path,'r') as f1:
+    with open(content_path,'r') as f1:
         lines = f1.readlines()
 
         '''
         etc……
         '''
     x = []
+    for i in range(len(lines)):
+        x.append(lines[i].split('\t')[1:-1])
     return x
+# load_data()
 
-
-def LSTM():
+# def LSTM():
