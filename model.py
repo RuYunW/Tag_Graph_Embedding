3b0375685f69d1c90c0ef29f703fec61fe807df3
汝芸 <ruyunw@163.com>
2019-08-27 01:12:55 +0800

    hhh3

--- model.py
+++ model.py
@@ -3,17 +3,42 @@ Tag-graph embedding generation algorithm
 '''
 
 from utils import load_data
+import numpy as np
 
 # Initialize
-x = load_data()
-order = []  # 创建一个空集合
+x,cites,content,class_set = load_data()
+order = []  # Create an empty list
 K = 5  # hops
 h = [[] for i in range(K+1)]  # temp matrices
-# print(h)
 # print(x[0][0])  # [[0,...,0],[...],...,[...]]
-for i in x:
-    h[0].append(i)
 
+h[0]+=x[:]
+
+# print(np.array(x)[:,0])
+# print(h[0][0][0])
+temp_frontnode = []
+temp_frontcode = []
+front_node = []
+front_code = []
+
+for node_id in np.array(x)[:,0]:
+    for line in cites:
+        if line[1] == node_id:
+            temp_frontnode.append(line[0])
+            # print(666)
+    for line in content:
+        if line[0] in temp_frontnode:
+            temp_frontcode.append(line[:])
+            # print(777)
+    front_node.append(temp_frontnode)
+    front_code.append(temp_frontcode)
+    temp_frontnode = []
+    temp_frontcode = []
+
+print(front_node)
+print(front_code[0])
+
+# print(class_set)
 # len(h[]=2708) = n(node)   len(h[][] = 1433) = n(words) -- feature matrices
 
 # for k in range(K):  # hop数  K
--- utils.py
+++ utils.py
@@ -1,15 +1,29 @@
+import numpy as np
 
 def load_data(cites_path = './data/cora.cites',content_path = './data/cora.content'):
     with open(content_path,'r') as f1:
         lines = f1.readlines()
-
-        '''
-        etc……
-        '''
     x = []
     for i in range(len(lines)):
-        x.append(lines[i].split('\t')[1:-1])
-    return x
+        x.append(lines[i].split('\t')[0:-1])
+
+    cites = []
+    with open(cites_path,'r') as f_cites:
+        lines = f_cites.readlines()
+    for line in lines:
+        cites.append(line.replace('\n','').split('\t'))
+
+    content = []
+    with open(content_path,'r') as f_co:
+        lines = f_co.readlines()
+    for line in lines:
+        content.append(line.replace('\n','').split('\t'))
+
+    class_set = set(np.array(content[:])[:,-1])
+    return x,cites,content,class_set
 # load_data()
 
 # def LSTM():
+
+def Virtualized():
+    pass
